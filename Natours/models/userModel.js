7e625304f8451d0195c357f7d3f9175db49defec
lasse <lasse.baun@outlook.com>
2019-10-08 14:45:45 +0200

    Implemented signup

--- Natours/models/userModel.js
+++ Natours/models/userModel.js
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema({
 		type: String,
 		required: [true, 'Please provide a password!'],
 		minlength: 8
+	},
+	passwordConfirm: {
+		type: String,
+		required: [true, 'Please confirm your password']
 	}
 })
 
