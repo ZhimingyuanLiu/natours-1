580f3f9e28a63b647918d215f57c8d30db606d07
lasse <lasse.baun@outlook.com>
2019-10-14 17:52:06 +0200

    Update username + email

--- Natours/controllers/userController.js
+++ Natours/controllers/userController.js
@@ -1,5 +1,17 @@
 const User = require('./../models/userModel')
+const AppError = require('./../utils/AppError')
 const catchAsync = require('./../utils/catchAsync')
+
+const filterObj = (obj, ...alllowedFields) => {
+	const newObj = {}
+
+	// Loop throw an object
+	Object.keys(obj).forEach(el => {
+		if (alllowedFields.includes(el)) newObj[el] = obj[el]
+	})
+
+	return newObj
+}
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
 	//* EXECUTE QUERY */
@@ -15,14 +27,14 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
 	})
 })
 
-exports.createUser = (req, res) => {
+exports.getUser = (req, res) => {
 	res.status(500).json({
 		status: 'error',
 		message: 'This route is not yet defined!'
 	})
 }
 
-exports.getUser = (req, res) => {
+exports.createUser = (req, res) => {
 	res.status(500).json({
 		status: 'error',
 		message: 'This route is not yet defined!'
@@ -42,3 +54,23 @@ exports.deleteUser = (req, res) => {
 		message: 'This route is not yet defined!'
 	})
 }
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+	//* 1) Create error if user POSTs password data
+	if (req.body.passwordCurrent || req.body.password || req.body.passwordConfirm) {
+		return next(new AppError('This route is not for password updates. Please use /updateMyPassword', 400))
+	}
+
+	//* 2) Filter out unwanted field names that aren't allowed to be updated
+	const filteredBody = filterObj(req.body, 'name', 'email')
+
+	//* 3) Update user document
+	const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, { new: true, runValidators: true })
+
+	res.status(200).json({
+		status: 'success',
+		data: {
+			user: updatedUser
+		}
+	})
+})
