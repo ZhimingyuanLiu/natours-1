1898d122670466cc85ec59364338d364632d2b1e
lasse <lasse.baun@outlook.com>
2019-10-14 19:23:37 +0200

    JWT via cookie & rate limiting

--- Natours/app.js
+++ Natours/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
 
 const AppError = require('./utils/AppError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -8,10 +9,18 @@ const userRouter = require('./routes/userRoutes')
 
 const app = express()
 
-// 1) MIDDLEWARES
+//* 1) GLOABAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'))
 }
+
+// 100 request from the same IP in 1 hour
+const limiter = rateLimit({
+	max: 3,
+	windowMs: 60 * 60 * 1000,
+	message: 'Too many requests from this IP, please try again in an hour.'
+})
+app.use('/api', limiter)
 
 app.use(express.json())
 
@@ -21,15 +30,16 @@ app.use((req, res, next) => {
 	next()
 })
 
-// 2) ROUTES
+//* 2) ROUTES
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 
-// 3) 404-Error
+//* 3) 404-Error
 app.all('*', (req, res, next) => {
 	next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
 })
 
+//* 4) ERROR-HANDLER
 app.use(globalErrorHandler)
 
 module.exports = app
--- Natours/controllers/authController.js
+++ Natours/controllers/authController.js
@@ -15,6 +15,15 @@ const signToken = id => {
 const createSendToken = (user, showUserData, res, statusCode, req) => {
 	const token = signToken(user._id)
 
+	const cookieOptions = {
+		expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000), // convert to milliseconds (was days before)
+		httpOnly: true // Cannot be accessed or modified
+	}
+
+	if (process.env.NODE_ENV === 'production') cookieOptions.secure = true // Will only be send on encrypted connection (https)
+
+	res.cookie('jwt', token, cookieOptions)
+
 	if (showUserData) {
 		res.status(statusCode).json({
 			status: 'success',
