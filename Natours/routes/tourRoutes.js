253fc2e0b036cca03ef0b14cb22b26f8758fdb05
lasse <lasse.baun@outlook.com>
2019-10-28 17:35:53 +0100

    First nested rout

--- Natours/routes/tourRoutes.js
+++ Natours/routes/tourRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const tourController = require('./../controllers/tourController')
 const authController = require('./../controllers/authController')
+const reviewRouter = require('./../routes/reviewRoutes')
 
 const router = express.Router()
 
-//router.param('id', tourController.checkId)
+//* Nested routes
+router.use('/:tourId/reviews', reviewRouter)
 
 router
 	.route('/')
