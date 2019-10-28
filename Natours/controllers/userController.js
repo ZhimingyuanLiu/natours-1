7120fc62223f657e833841292d913bb10a260acc
lasse <lasse.baun@outlook.com>
2019-10-28 19:45:21 +0100

    /me endpoint

--- Natours/controllers/userController.js
+++ Natours/controllers/userController.js
@@ -27,6 +27,11 @@ exports.getUser = factory.getOne(User)
 exports.updateUser = factory.updateOne(User) // Do NOT update passwords with this
 exports.deleteUser = factory.deleteOne(User)
 
+exports.getMe = (req, res, next) => {
+	req.params.id = req.user.id
+	next()
+}
+
 exports.updateMe = catchAsync(async (req, res, next) => {
 	//* 1) Create error if user POSTs password data
 	if (req.body.passwordCurrent || req.body.password || req.body.passwordConfirm) {
