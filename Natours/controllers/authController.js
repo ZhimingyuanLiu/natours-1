ecc117d410fbc186fa4ae6911084dc27f5ae3886
lasse <lasse.baun@outlook.com>
2019-10-31 00:04:23 +0100

    Logout button

--- Natours/controllers/authController.js
+++ Natours/controllers/authController.js
@@ -107,26 +107,38 @@ exports.protect = catchAsync(async (req, res, next) => {
 })
 
 // Only for rendered oages, no errors!
-exports.isLoggedIn = catchAsync(async (req, res, next) => {
+exports.isLoggedIn = async (req, res, next) => {
 	if (req.cookies.jwt) {
-		//* 1) Verify token
-		const decoded = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET)
+		try {
+			//* 1) Verify token
+			const decoded = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET)
 
-		//* 2) Check if user still exists
-		const currentUser = await User.findById(decoded.id)
-		if (!currentUser) return next()
+			//* 2) Check if user still exists
+			const currentUser = await User.findById(decoded.id)
+			if (!currentUser) return next()
 
-		//* 3) Check if user changed password after the token was issued
-		if (currentUser.changedPasswortAfter(decoded.iat)) {
+			//* 3) Check if user changed password after the token was issued
+			if (currentUser.changedPasswortAfter(decoded.iat)) {
+				return next()
+			}
+
+			//* There is a logged in user
+			res.locals.user = currentUser
+			return next()
+		} catch (err) {
 			return next()
 		}
-
-		//* There is a logged in user
-		res.locals.user = currentUser
-		return next()
 	}
 	next()
-})
+}
+
+exports.logout = (req, res) => {
+	res.cookie('jwt', 'logged ouuut', {
+		expires: new Date(Date.now() + 10 * 1000),
+		httpOnly: true
+	})
+	res.status(200).json({ status: 'success' })
+}
 
 exports.restrictTo = (...roles) => {
 	return (req, res, next) => {
