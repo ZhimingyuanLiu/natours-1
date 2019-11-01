24e1d74a67f7ac9163b581970b2fb29651ba0857
lasse <lasse.baun@outlook.com>
2019-11-01 15:29:26 +0100

    User image uploads

--- Natours/controllers/userController.js
+++ Natours/controllers/userController.js
@@ -1,7 +1,41 @@
+const multer = require('multer')
+const sharp = require('sharp')
 const User = require('./../models/userModel')
 const AppError = require('./../utils/AppError')
 const catchAsync = require('./../utils/catchAsync')
 const factory = require('./handlerFactory')
+
+//* MULTER */
+const multerStorage = multer.memoryStorage()
+const multerFilter = (req, file, cb) => {
+	if (file.mimetype.startsWith('image')) {
+		cb(null, true)
+	} else {
+		cb(new AppError('Not an image! Please upload only images.', 400), false)
+	}
+}
+const upload = multer({
+	storage: multerStorage,
+	fileFilter: multerFilter
+})
+
+exports.uploadUserPhoto = upload.single('photo')
+
+exports.resizeUserPhoto = (req, res, next) => {
+	// uploadUserPhoto puts the file on req.file
+	if (!req.file) return next()
+
+	req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`
+
+	//* Convert image */
+	sharp(req.file.buffer)
+		.resize(500, 500)
+		.toFormat('jpeg')
+		.jpeg({ quality: 90 })
+		.toFile(`public/img/users/${req.file.filename}`)
+
+	next()
+}
 
 //? Returns a new object which only contains "...allowedFields" properties
 const filterObj = (obj, ...alllowedFields) => {
@@ -40,6 +74,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
 	//* 2) Filter out unwanted field names that aren't allowed to be updated
 	const filteredBody = filterObj(req.body, 'name', 'email')
+	if (req.file) filteredBody.photo = req.file.filename
 
 	//* 3) Update user document
 	const updatedUser = await User.findByIdAndUpdate(req.user._id, filteredBody, { new: true, runValidators: true })
--- Natours/public/js/bundle.js
+++ Natours/public/js/bundle.js
@@ -8874,12 +8874,11 @@ if (logoutButton) logoutButton.addEventListener('click', _login.logout);
 if (userDataForm) {
   userDataForm.addEventListener('submit', function (e) {
     e.preventDefault();
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    (0, _updateSettings.updateSettings)({
-      name: name,
-      email: email
-    }, 'data');
+    var form = new FormData();
+    form.append('name', document.getElementById('name').value);
+    form.append('email', document.getElementById('email').value);
+    form.append('photo', document.getElementById('photo').files[0]);
+    (0, _updateSettings.updateSettings)(form, 'data');
   });
 }
 
@@ -8954,7 +8953,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51195" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59579" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
